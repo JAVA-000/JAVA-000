1859cf610400cb403c9de1e4e4349b8a76150358
qinhui0112 <32504141@qq.com>
2020-10-16 18:26:22 +0800

    #学号：G20200579010538 #姓名：秦会 #班组:4班 #小组：暂无 #作业：week01/2题 & 测试提交作业

---
+++ Week_01/JkdxJava0Class4QH/src/com/jk/jvm/week01/title02/PramInfo.java
@@ -0,0 +1,9 @@
+package com.jk.jvm.week01.title02;
+
+public class PramInfo {
+	
+	public static final String CLASSNAME_HELLO="Hello";
+	
+	public static final String METHOD_NAME_HELLO="hello";
+	
+}
