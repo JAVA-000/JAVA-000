7744eca930f57ee3fbad2d2ceb44812d7d0ab3af
qingyi.qxf <quxiaofeng@hundunyun.com.cn>
2020-11-05 00:57:29 +0800

    commit week03-practice1-1

---
+++ Week_03/gateway/src/main/java/io/github/kimmking/gateway/inbound/HttpInboundHandler.java
@@ -0,0 +1,43 @@
+package io.github.kimmking.gateway.inbound;
+
+import io.github.kimmking.gateway.outbound.okhttp.OkhttpOutboundHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.util.ReferenceCountUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HttpInboundHandler extends ChannelInboundHandlerAdapter {
+
+    private static Logger logger = LoggerFactory.getLogger(HttpInboundHandler.class);
+    private final String proxyServer;
+    /**
+     * myOutboundHandler
+     */
+    private OkhttpOutboundHandler handler;
+
+    public HttpInboundHandler(String proxyServer) {
+        this.proxyServer = proxyServer;
+        handler = new OkhttpOutboundHandler(this.proxyServer);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) {
+        ctx.flush();
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        try {
+            FullHttpRequest fullRequest = (FullHttpRequest) msg;
+            handler.handle(fullRequest, ctx);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            ReferenceCountUtil.release(msg);
+        }
+    }
+
+
+}
