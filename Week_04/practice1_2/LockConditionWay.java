ce1fb50844f897778849b0019677167a3d9de253
qingyi.qxf <quxiaofeng@hundunyun.com.cn>
2020-11-12 00:47:56 +0800

    Week04 commit

---
+++ Week_04/practice1_2/LockConditionWay.java
@@ -0,0 +1,62 @@
+package practice1_2;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 本周作业：（必做）思考有多少种方式，在main函数启动一个新线程或线程池，
+ * 异步运行一个方法，拿到这个方法的返回值后，退出主线程？
+ * 写出你的方法，越多越好，提交到github。
+ * <p>
+ * LockConditionWay
+ */
+public class LockConditionWay {
+
+    private static Integer result = null;
+    private Lock lock = new ReentrantLock();
+    Condition condition = lock.newCondition();
+
+    public void lockSum() {
+        lock.lock();
+        try {
+            result = sum();
+            condition.signal();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+
+        long start = System.currentTimeMillis();
+        // 在这里创建一个线程或线程池，
+        // 异步执行 下面方法
+        Thread thread = new Thread(() -> new LockConditionWay().lockSum());
+        thread.start();
+        thread.join();
+
+        //这是得到的返回值
+
+        // 确保  拿到result 并输出
+        System.out.println("异步计算结果为：" + result);
+
+        System.out.println("使用时间：" + (System.currentTimeMillis() - start) + " ms");
+
+        // 然后退出main线程
+    }
+
+
+    private static int sum() {
+        return fibo(36);
+    }
+
+    private static int fibo(int a) {
+        if (a < 2)
+            return 1;
+        return fibo(a - 1) + fibo(a - 2);
+    }
+}
